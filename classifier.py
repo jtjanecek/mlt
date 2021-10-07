15867a4940d65bc7c01a515c6be00687b67fcb5e
John Janecek <janecektyler@gmail.com>
2021-10-06 20:55:29 -0600

    Updates

--- classifier.py
+++ classifier.py
@@ -6,7 +6,9 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import accuracy_score
 
+from scipy.stats import zscore
 
+import pandas as pd
 import matplotlib as mpl
 mpl.rcParams['figure.dpi'] = 150
 mpl.rcParams.update({'font.size': 5})
@@ -28,7 +30,7 @@ class Classifier:
 			raise Exception(f"Found {len(set(self._y))} possible outcomes, but expected 2. Outcomes: {set(self._y)}")
 
 		self._rf_importance()
-		#self._lg_importance()
+		self._lg_importance()
 
 
 	def _rf_importance(self):
@@ -38,15 +40,44 @@ class Classifier:
 		feature_importance = 100 * (model.feature_importances_ / max(model.feature_importances_))
 		logger.info(self._columns)
 		logger.info(feature_importance)
-		pi = permutation_importance(model, self._X, self._y, n_jobs=self._n_cores, n_repeats=100, random_state=self._random_state)['importances_mean']
-		logger.info(pi)
+		pi = permutation_importance(model, self._X, self._y, n_jobs=self._n_cores, n_repeats=100, random_state=self._random_state)
+
+		# Save to CSVs
+		rf_out_dict = {'feature': self._columns, 'gini_impurity_importance': feature_importance}
+		rf_df = pd.DataFrame(rf_out_dict)
+
+		rf_pi_dict = pd.DataFrame(pi['importances'])
+		rf_pi_dict['feature'] = self._columns
+
 		logger.info("Saving ...")
+		rf_df.to_csv(f'{self._name}_rf_gini_importance.csv',index=False)
+		rf_pi_dict.to_csv(f'{self._name}_rf_permutation_importance.csv',index=False)
 		
-		ypred = model.predict(self._X)
-		logger.info(accuracy_score(self._y, ypred))
+	def _lg_importance(self):
+		logger.info("Calculating LogisticRegression Coefficient Feature Importance ...")
+		X_z = pd.DataFrame(self._X).apply(zscore).values
+
+		model = LogisticRegression(random_state=self._random_state, n_jobs=self._n_cores)		
+		model.fit(X_z,self._y)
+
+		'''
+		logger.info(self._columns)
+		logger.info(feature_importance)
+		pi = permutation_importance(model, self._X, self._y, n_jobs=self._n_cores, n_repeats=100, random_state=self._random_state)
+
+		# Save to CSVs
+		rf_out_dict = {'feature': self._columns, 'coefficients': feature_importance}
+		rf_df = pd.DataFrame(rf_out_dict)
+
+		rf_pi_dict = pd.DataFrame(pi['importances'])
+		rf_pi_dict['feature'] = self._columns
+
+		logger.info("Saving ...")
+		rf_df.to_csv(f'{self._name}_rf_gini_importance.csv',index=False)
+		rf_pi_dict.to_csv(f'{self._name}_rf_permutation_importance.csv',index=False)
+		'''
 		
-	
-		
+
 			
 
 '''
